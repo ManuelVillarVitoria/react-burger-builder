a899b8b08107f4374831cb7e49754e26dfdfb930
manu <manu.villar.vitoria@gmail.com>
2020-09-09 18:24:46 +0200

    creating the Contact Data component & displaying it clicking 'continue' in the Checkout page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 
 class Checkout extends Component {
@@ -13,7 +15,7 @@ class Checkout extends Component {
         }
     }
 
-    componentWillMount() {
+    UNSAFE_componentWillMount() {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
 
@@ -40,6 +42,10 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients} 
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
+                />
+                <Route 
+                    path={ this.props.match.path + '/contact-data' } 
+                    component={ContactData} 
                 />
             </div>
         );
